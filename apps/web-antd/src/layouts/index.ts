34bced483e0fdd3b10f9b87617a6f5d4ff70b31d
🍀 <50286230+XERA-2011@users.noreply.github.com>
2025-12-31 10:11:25 +0800

    feat: Remove the `web-antd`

--- apps/web-antd/src/layouts/index.ts
+++
@@ -1,7 +0,0 @@
-const BasicLayout = () => import('./basic.vue');
-const AuthPageLayout = () => import('./auth.vue');
-
-const IFrameView = () =>
-  import('@x-monorepo/layouts').then((m) => m.IFrameView);
-
-export { AuthPageLayout, BasicLayout, IFrameView };
--- apps/web-antd/src/views/_core/about/index.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import { About } from '@x-monorepo/common-ui';
-
-defineOptions({ name: 'About' });
-</script>
-
-<template>
-  <About />
-</template>
